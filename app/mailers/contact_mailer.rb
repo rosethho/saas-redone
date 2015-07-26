a9e023057a12a44d159c5742711553a896f16dac
frankie howland <robertsethh@yahoo.com>
2015-07-26 18:19:18 +0000

    Added mailer and view

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,11 @@
+class CreateMailer < ActionMailer::Base
+    default to: 'robettsethh@yahoo.com'
+    
+    def contact_email(name, email, body)
+        @name = name
+        @email = email
+        @body = body
+        
+        mail(from: email, subject: 'Contact Form Message')
+    end
+end
